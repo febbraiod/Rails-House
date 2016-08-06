d93cac3425255c84343ad87f9c86642438aef356
febbraiod <donedesign1@gmail.com>
2016-08-06 13:48:25 -0400

    more sophisticated flickering lights, added assests for crossing effect

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,49 @@
 
 function flicker_on(){
-  console.log('sec');
-  $("#home").hide();
+  console.log('on');
   $("#home_back_light").show();
+  $("#home_back_dim").hide();
+  $("#home_back_mid").hide();
 }
 
 function flicker_off(){
-  $("#home").show();
+  console.log('off');
   $("#home_back_light").hide();
+  $("#home_back_dim").hide();
+  $("#home_back_mid").hide();
 }
 
+function flicker_dim(){
+  console.log('dim');
+  $("#home_back_dim").show();
+  $("#home_back_mid").hide();
+}
+
+function flicker_mid(){
+  console.log('mid');
+  $("#home_back_mid").show();
+  $("#home_back_dim").hide();
+}
+
+function setIntervals(){
+  var a = setInterval(flicker_on, 590);
+  var b = setInterval(flicker_dim, 520);
+  var c = setInterval(flicker_mid, 510);
+  var d = setInterval(flicker_dim, 540);
+  var e = setInterval(flicker_off, 660);
+
+  function clear(){
+    clearInterval(a);
+    clearInterval(b);
+    clearInterval(c);
+    clearInterval(d);
+    clearInterval(e);
+  }
+
+  setTimeout(clear, 3700);
+}
+
+
 function startTimer(){
-  setInterval(flicker_on, 1000);
-  setInterval(flicker_off, 1050);
+  setTimeout(setIntervals, 200);
 }
