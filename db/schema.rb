75147e9035233751719cb89a8357d14da0e66786
febbraiod <donedesign1@gmail.com>
2016-08-08 19:03:53 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160320160031) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bags", force: :cascade do |t|
     t.integer  "player_id"
     t.datetime "created_at", null: false
