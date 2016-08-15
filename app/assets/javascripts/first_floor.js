5211346319051b16704901a7b84cf18c50cd858d
febbraiod <donedesign1@gmail.com>
2016-08-15 17:35:53 -0400

    add art for note on front door as well as highlight functionality

--- app/assets/javascripts/first_floor.js
+++ app/assets/javascripts/first_floor.js
@@ -1,17 +1,28 @@
 $(function(){
-  bind_package();
+  bindEntryHighlights();
 });
 
-function bind_package(){
-  $('#package_div').hover(showHighlight, hideHighlight);
+function bindEntryHighlights(){
+  $('#package_div').hover(showPackageHighlight, hidePackageHighlight);
+  $('#noteondoor_div').hover(showNoteHighlight, hideNoteHighlight);
 }
 
-function showHighlight(){
+function showPackageHighlight(){
   $('#package').hide();
   $('#package_highlight').show();
 }
 
-function hideHighlight(){
+function hidePackageHighlight(){
   $('#package_highlight').hide();
   $('#package').show();
+}
+
+function showNoteHighlight(){
+  $('#noteondoor').hide();
+  $('#noteondoor_highlight').show();
+}
+
+function hideNoteHighlight(){
+  $('#noteondoor_highlight').hide();
+  $('#noteondoor').show();
 }
